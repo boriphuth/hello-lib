3ca75621a339583cac02e4266e4a3299d52fc200
chavp <my.parinya@gmail.com>
2016-01-20 15:00:01 +0700

    add first hello-lib to github
    Signed-off-by: chavp <my.parinya@gmail.com>

---
+++ test/HelloLib.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloLib.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
